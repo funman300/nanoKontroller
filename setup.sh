95c2392bb8b35c2b522ff8a6ff9cd53e0f29246a
Fun <funman300@gmail.com>
2024-01-08 15:03:02 -0800

    Update setup.sh
    simplified the setup script

--- setup.sh
+++ setup.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
+# Define variables
+systemd_user_dir="$HOME/.config/systemd/user"
+service_file="nanoKontroller.service"
+config_file="nanoKontroller.ini"
+package_manager="paru"  # Set your package manager here (e.g., pacman, apt, dnf)
+
 # Install required packages
-yay -S --noconfirm python-mido python-rtmidi python-uinput python-evdev python-pulsectl
+sudo "$package_manager" -S --noconfirm python-mido python-rtmidi python-uinput python-evdev python-pulsectl
 
 # Create systemd user directory if it doesn't exist
-systemd_user_dir="$HOME/.config/systemd/user"
 mkdir -p "$systemd_user_dir"
 
-# Copy service file
-cp nanoKontroller.service "$systemd_user_dir/"
+# Copy service file to systemd user directory
+cp "$service_file" "$systemd_user_dir/"
 
-# Copy nanoKontroller.ini to ~/.config
-cp nanoKontroller.ini "$HOME/.config/"
+# Copy config file to ~/.config/
+cp "$config_file" "$HOME/.config/"
 
 # Copy files with sudo permissions
 sudo cp uinput.conf /etc/modules-load.d/
 sudo cp 01-uinput-permission.rules /etc/udev/rules.d/
 
-# Enable the service
-systemctl --user enable nanoKontroller.service
+# Enable and start the service
+systemctl --user enable --now "$service_file"
 
-echo "Please reboot to use the device"
+echo "Installation completed. Please reboot to use the device."
